863ef61a2dbb99a39a320cfce559830632b024d4
Lars Schöbitz <lschoebitz@ethz.ch>
2024-03-11 14:46:54 +0100

    export data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,22 @@
+
+
+
+library(tidyverse)
+library(openwashdata)
+library(janitor)
+
+# import data -------------------------------------------------------------
+
+data_in <- read_csv("data-raw/wash-dev-mdb_20221223_data.csv")
+
+data_out <- data_in |>
+  clean_names() |>
+  glimpse()
+
+
+
+# export data -------------------------------------------------------------
+
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(data_out, overwrite = TRUE)
