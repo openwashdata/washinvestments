e9bc008c30c05e0544ed9f48c2a3dabfee082c36
Margaux Götschmann <margaux.goetschmann@gmail.com>
2024-05-08 13:33:20 +0200

    Add dictionary

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,27 +1,33 @@
+# Description -------------------------------------------------------------
 
+# R script to process uploaded raw data into a tidy, analysis-ready data frame
 
+# Load packages -----------------------------------------------------------
 
+library(readxl)
 library(tidyverse)
 library(openwashdata)
 library(janitor)
+library(dplyr)
 
-# import data -------------------------------------------------------------
+# Read data -------------------------------------------------------------
 
-data_in <- read_csv("data-raw/wash-dev-mdb_20221223_data.csv")
+data_in <- read_csv("data-raw/wash-dev-mdb_20221223_data.csv") |>
+  as_tibble()
 
 washinvestments <- data_in |>
-  clean_names() |>
-  glimpse()
+  clean_names()
 
-washinvestments |>
-  count(region)
+# Tidy data ---------------------------------------------------------------
 
-washinvestments |>
-  count(multi_country)
+# Modify variables' types
 
+washinvestments <- washinvestments |>
+  mutate(non_network_infrastructure = as.logical(non_network_infrastructure))
 
-# export data -------------------------------------------------------------
-
-## code to prepare `DATASET` dataset goes here
+# Write data -------------------------------------------------------------
 
 usethis::use_data(washinvestments, overwrite = TRUE)
+fs::dir_create(here::here("inst", "extdata"))
+write_csv(washinvestments, here::here("inst", "extdata", "washinvestments.csv"))
+openxlsx::write.xlsx(washinvestments, here::here("inst", "extdata", "washinvestments.xlsx"))
