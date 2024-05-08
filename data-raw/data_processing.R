d35073bc18129bfdfd14d5f990218b823bba1c4d
Margaux Götschmann <margaux.goetschmann@gmail.com>
2024-05-08 14:22:14 +0200

    Export data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,32 +1,36 @@
 # Description -------------------------------------------------------------
-
 # R script to process uploaded raw data into a tidy, analysis-ready data frame
 
-# Load packages -----------------------------------------------------------
+# Install package for detecting and deleting non-UTF8 entries -------------
+#install.packages("remotes")
+#remotes::install_github("shug0131/cctu")
 
+# Load packages -----------------------------------------------------------
 library(readxl)
 library(tidyverse)
 library(openwashdata)
 library(janitor)
 library(dplyr)
+library(cctu)
 
 # Read data -------------------------------------------------------------
-
 data_in <- read_csv("data-raw/wash-dev-mdb_20221223_data.csv") |>
   as_tibble()
 
-washinvestments <- data_in |>
+# Tidy data ---------------------------------------------------------------
+# Remove non-UTF8 entries
+cctu::detect_invalid_utf8(data_in)
+data <- cctu::remove_invalid_utf8(data_in)
+
+# Adjust variables' names
+washinvestments <- data |>
   clean_names()
 
-# Tidy data ---------------------------------------------------------------
-
 # Modify variables' types
-
 washinvestments <- washinvestments |>
   mutate(non_network_infrastructure = as.logical(non_network_infrastructure))
 
 # Write data -------------------------------------------------------------
-
 usethis::use_data(washinvestments, overwrite = TRUE)
 fs::dir_create(here::here("inst", "extdata"))
 write_csv(washinvestments, here::here("inst", "extdata", "washinvestments.csv"))
